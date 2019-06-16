32fda22bb9d10a49b213114fb2211319811b7b1f
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-16 17:33:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,16 @@
 #Yesilyurt 
 
+class Shoe 
+
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand 
+
+    def initialize(brand)
+        @brand = brand 
+    end 
+
+    def cobble
+        puts "The shoe has been repaired."
+    end 
+
+end 
